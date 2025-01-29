598aee9b4ea88c0411a2fac15bc7c235307cb49f
Dileep T <108917385+dileep99999@users.noreply.github.com>
2025-01-30 01:55:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
